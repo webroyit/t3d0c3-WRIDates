1a11b6ccb4c25ea51ca1638ef2df75d99ea685cd
royweb <xytank@gmail.com>
2021-03-04 15:27:03 -0500

    Fetch list of people from firestore

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from "react-tinder-card";
 
+import database from '../firebase';
 import './TinderCards.css';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Kevin',
-            url: 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=751&q=80'
-        },
-        {
-            name: 'Lauren',
-            url: 'https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjJ8fHBlb3BsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-        }
-    ]);       // => 'const people = []'
+    const [people, setPeople] = useState([]);       // => 'const people = []'
 
     // (Push to an array in React)
     // ... is called spread operator that keep the copy of people and add new value to it
     // setPeople([...people, "Kevin", "Lauren"]); => people.push('Kevin', 'Lauren)
+
+    // Piece of code which runs based on a condition
+    useEffect(() => {
+        // Get all people from firebase database
+        // onSnapshot() is a listener
+        database.collection('people').onSnapshot(snapshot => {
+            setPeople(snapshot.docs.map(doc => doc.data()));
+        });
+
+        // This will run once when component loads if the array is empty
+    }, [])
 
     return (
         <div>
